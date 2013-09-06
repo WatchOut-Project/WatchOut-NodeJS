20e1c5c05e987db043b822a8d23cc3077b844898
TheFinestArtist <contact@thefinestartist.com>
2013-09-06 12:20:22 +0900

    WIP

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -5,13 +5,67 @@
  * @description	:: Contains logic for handling requests.
  */
 
+var passport = require('passport');
+
 module.exports = {
 
-  /* e.g.
-  sayHello: function (req, res) {
-    res.send('hello world!');
-  }
-  */
-  
+	index: function (req,res)
+	{
+		res.view();
+	},
+ 
+	process: function(req, res)
+	{
+		passport.authenticate(
+			'local',
+			function(err, user, info)
+			{
+				if ((err) || (!user))
+				{
+					res.cookie('error', info.message);
+					res.redirect('/login');
+					return;
+				}
+	 
+				req.logIn(
+					user,
+					function(err)
+					{
+						if (err)
+						{
+							res.cookie('error', err);
+							res.redirect('/login');
+							return;
+						}
+						
+						res.redirect('/');
+						return;
+					}
+				);
+			}
+		)(req, res);
+	},
+ 
+ 	login: function (req, res)
+ 	{
+		res.view();
+ 	},
+ 	
+	logout: function (req,res)
+	{
+		req.logout();
+		res.redirect('/login');
+	},
+
+	register: function (req, res)
+	{
+		if(req.isAuthenticated())
+		{
+			res.cookie('info', "You can't register while logged in.");
+			res.redirect('/');
+			return;
+		}
+		res.view();
+	}  
 
 };
