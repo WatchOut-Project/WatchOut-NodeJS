efea6fdcea960e849fd8065a5137d9f5ef869262
TheFinestArtist <liger745547@gmail.com>
2013-09-08 17:14:41 +0900

    Added bcrypt

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -7,34 +7,34 @@
 
 module.exports = {
 
-  create: function(req, res)
-	{
-		if(req.body)
-		{
-			req.body.password = Users.hashPass(req.body.password);
-			Users.create(req.body).done(
-				function(err, user)
-				{
-					if (err)
-					{
-						res.cookie('error', "There was an error" + err);
-						res.redirect('/register');
-						return;
-					}
-					else
-					{
-						res.cookie('info', "You have succesfully registered.");
-						res.redirect('/login');
-						return;
-				  	}
-				}
-			);
-		}
-		else
-		{
-			res.redirect('/login');
-			return;
-		}
-	}
+ //  create: function(req, res)
+	// {
+	// 	if(req.body)
+	// 	{
+	// 		req.body.password = Users.hashPass(req.body.password);
+	// 		Users.create(req.body).done(
+	// 			function(err, user)
+	// 			{
+	// 				if (err)
+	// 				{
+	// 					res.cookie('error', "There was an error" + err);
+	// 					res.redirect('/register');
+	// 					return;
+	// 				}
+	// 				else
+	// 				{
+	// 					res.cookie('info', "You have succesfully registered.");
+	// 					res.redirect('/login');
+	// 					return;
+	// 			  	}
+	// 			}
+	// 		);
+	// 	}
+	// 	else
+	// 	{
+	// 		res.redirect('/login');
+	// 		return;
+	// 	}
+	// }
 
 };
--- api/models/Users.js
+++ api/models/Users.js
@@ -41,7 +41,7 @@ module.exports = {
 
     toJSON: function() {
       var obj = this.toObject();
-      delete obj.pass;
+      delete obj.password;
       return obj;
     }
 
